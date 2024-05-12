3503d8eae1fef35878afe0616cc5e8aea9f4667b
Rajesh surmeda <160104433+Rajsurmeda@users.noreply.github.com>
2024-05-12 18:26:21 +0500

    add my atm machine

---
+++ atm.js
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 20000;
+let myPin = 33442;
+let pinAnswer = await inquirer.prompt({
+    name: "pincode",
+    type: "number",
+    message: "Enter Your Pin"
+});
+if (pinAnswer.pincode === myPin) {
+    console.log("Correct Pin Code!!!");
+    let operationAns = await inquirer.prompt([{
+            name: "operation",
+            message: "Please Select Option",
+            type: "list",
+            choices: ["withdraw", "check balance",]
+        }]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([{
+                name: "amount",
+                message: "Enter Your Amount",
+                type: "number"
+            }]);
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("Your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect Pin Number");
+}
+// import inquirer from "inquirer"
+// let myblance= 1000;
+// let mypin= 44537;
+// const atm=await inquirer.prompt(
+//     [
+//         {name:"pin",
+//         message:"enter your pin",
+//         type:"number"
+//         }
+//     ]
+//     );
+//     if (atm.pin==="mypin"){
+//         console.log("correct pin code!!!");
+//     };
+//     let operationans=await inquirer.prompt
+//     (
+//         [
+//         {name:"operation",
+//         message:"please select option",
+//         type:'list',
+//         choices:["withdraw","checkblance"]
+//         }
+//     ]
+//     );
+//     console.log(operationans);
+//     if(operationans.operation==="withdraw"){
+//        let amountans=await inquirer.prompt([{
+//         name:"operation",
+//         Message:"please select option",
+//         type:"list",
+//         choices:["withdraw","checkblance"]
+//        }])
+// }
+// // console.log(operationans);
+// if(operationans.operation==="withdraw"){
+//     let amountans= await inquirer.prompt(
+//         [
+//             {name:"amount",
+//             Message:"enter your amount",
+//             type:"list",
+//             choices:["withdraw","checkblance","insufficint blance"]
+//             }
+//         ]
+//     )
+// }
+// // console.log(operationans);
+// if(operationans==="withdraw"){
+// }else{
+//     console.log("incorrect pin number");
+// }
